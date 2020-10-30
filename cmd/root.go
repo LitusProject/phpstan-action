01141f26d0ce00a39e8e0649cdb098aaef2b936c
Pieter Maene <pieter.maene@litus.cc>
2020-10-30 09:47:16 +0100

    Communicate results through workflow commands instead of the API

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -12,24 +12,16 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"golang.org/x/text/feature/plural"
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
-	"golang.org/x/text/message/catalog"
 )
 
 var (
-	ghRepository string
-	ghSHA        string
-	ghToken      string
-	ghWorkspace  string
+	ghWorkspace string
 
 	rootCmd = &cobra.Command{
 		Use:          "phpstan-action",
 		Short:        "PHPStan Action",
 		SilenceUsage: true,
 		RunE:         runRoot,
-		PreRunE:      runRootPre,
 	}
 )
 
@@ -41,27 +33,6 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	rootCmd.Flags().StringVar(
-		&ghRepository,
-		"github.repository",
-		"",
-		"owner and repository name",
-	)
-
-	rootCmd.Flags().StringVar(
-		&ghSHA,
-		"github.sha",
-		"",
-		"commit hash that triggered the workflow",
-	)
-
-	rootCmd.Flags().StringVar(
-		&ghToken,
-		"github.token",
-		"",
-		"installation access token for the job",
-	)
 
 	rootCmd.Flags().StringVar(
 		&ghWorkspace,
@@ -93,62 +64,13 @@ func runRoot(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	c, err := internal.NewClient()
+	ms, err := r.CreateMessages()
 	if err != nil {
 		return err
 	}
 
-	cr, err := c.CreateCheckRun()
-	if err != nil {
-		return err
-	}
-
-	as, err := r.CreateCheckRunAnnotations()
-	if err != nil {
-		return err
-	}
-
-	if err := c.UpdateCheckRun(cr, as); err != nil {
-		return err
-	}
-
-	if err := c.CompleteCheckRun(cr, as); err != nil {
-		return err
-	}
-
-	if len(as) > 0 {
-		p := message.NewPrinter(language.English)
-		e := p.Sprintf("phpstan has identified %d issue(s)", len(as))
-
-		return errors.New(e)
-	}
-
-	return nil
-}
-
-func runRootPre(cmd *cobra.Command, args []string) error {
-	{
-		err := message.Set(
-			language.English,
-			"PHPStan has identified %d issue(s).",
-			catalog.Var("issues", plural.Selectf(1, "", plural.One, "issue", plural.Other, "issues")),
-			catalog.String("PHPStan has identified %[1]d ${issues}."),
-		)
-
-		if err != nil {
-			return err
-		}
-	}
-
-	{
-		err := message.Set(
-			language.English,
-			"phpstan has identified %d issue(s)",
-			catalog.Var("issues", plural.Selectf(1, "", plural.One, "issue", plural.Other, "issues")),
-			catalog.String("phpstan has identified %[1]d ${issues}"),
-		)
-
-		if err != nil {
+	for _, m := range ms {
+		if _, err := fmt.Fprintln(os.Stdout, m); err != nil {
 			return err
 		}
 	}
